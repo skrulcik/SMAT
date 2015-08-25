d99407aa8c0aad0d89b9959ba0a6dbaf7b061922
Scott Krulcik <skrulcik@andrew.cmu.edu>
2015-08-25 01:40:38 +0000

    Improved weather colors
      - Mapping model over if-else model
      - Greater variety in colors, one for every 10 degrees

--- public/js/weather_widget.js
+++ public/js/weather_widget.js
@@ -1,33 +1,51 @@
 // Docs at http://simpleweatherjs.com
-
 function refreshWeatherInfo() {
     $.simpleWeather({
         zipcode: '15217',
         location: 'Pittsburgh',
         unit: 'f',
         success: function(weather) {
-            if(weather.temp > 80) {
-                $('.weather_color').addClass('red lighten-1');
-            } else if (weather.temp > 65) {
-                $('.weather_color').addClass('yellow lighten-2');
-            } else if (weather.temp > 30) {
-      	        $('.weather_color').addClass('indigo');
-            } else {
-      	        $('.weather_color').addClass('light-blue lighten-1');
-            }
+
+            setWeatherColorForTemperature(weather.temp);
+            
             var html = '<h1 class="icon-' + weather.code + '"></h1>';
             html += '<h2>' + weather.temp + '&deg;</h2>';
             html += '<h5>' + weather.currently + '</h5>';
-
-            // Show time of last update
             var timestamp = moment(weather.updated);
             html += '<p class="updated">Updated ' + moment(timestamp).fromNow() + '</p>';
-
             $('#weather').html(html);
+
         }, error: function(error) {
             $('#weather').html('<p><emph>Error Fetching Weather:</emph>\n' + error + '</p>');
         }
     });
+}
+
+var mappedWeatherColors = [
+    'blue-grey',    // < 10
+    'purple',       // 10 - 19
+    'indigo',       // 20 - 29
+    'blue',         // 30 - 39
+    'cyan',         // 40 - 49
+    'teal',         // 50 - 59
+    'green',        // 60 - 69
+    'yellow',       // 70 - 79
+    'orange',       // 80 - 89
+    'red',          // > 90
+]
+var allWeatherColorClasses = mappedWeatherColors.join(' ');
+function setWeatherColor(color) {
+    if (!$('.weather_color').hasClass('darken-2')) {
+        $('.weather_color').addClass('darken-2');
+    }
+    $('.weather_color').removeClass(allWeatherColorClasses).addClass(color);
+}
+function setWeatherColorForTemperature(temp) {
+    var mappedTemp = Math.floor(temp/10);
+    mappedTemp = Math.min(mappedTemp, mappedWeatherColors.length - 1);
+    mappedTemp = Math.max(mappedTemp, 0);
+    setWeatherColor(mappedWeatherColors[mappedTemp]);
+
 }
 
 $(document).ready(function() {
